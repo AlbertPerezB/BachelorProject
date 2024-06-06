26b6e2f0055669d8776cf16de0910739517419d8
AlbertPerezB <apb99@live.com>
2024-06-06 18:07:53 +0200

    HiveMQConfig moved

---
+++ Bachelor.MQTT.Shared/HiveMQConfig.cs
@@ -0,0 +1,8 @@
+namespace Bachelor.MQTT.Shared;
+public class HiveMQConfig
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Server { get; set; } = string.Empty;
+    public int Port { get; set; }
+}
